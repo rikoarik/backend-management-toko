7e9a23cb48c19b7713f7656e6758dc54c5df14e3
Arik Riko Prasetya <rikoarik04@gmail.com>
2026-05-18 10:33:10 +0700

    refactor: Remove NormalizeApiToken middleware from application configuration to streamline API token handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\NormalizeApiToken;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,10 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            NormalizeApiToken::class,
-        ]);
-
         $middleware->validateCsrfTokens(except: [
             'deploy/migrate',
         ]);
